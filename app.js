1c086af5aaf8e9afd6d8a588b13d3d4f1de7f881
Zilola <44432264+zilola08@users.noreply.github.com>
2023-10-29 08:34:25 +0500

    style changes, add more details

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const apiKey = "6d9a6f2f45891ae2a76eb65678bd8e0d";
 
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?&units=metric`;
+// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
 const weatherDiv = document.querySelector(".weather");
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 
 const weatherContent = `
+<h2 class="city">New York</h2>
 <img src="images/rain.png" class="weather-icon" alt="">
 <h1 class="temp">22°C</h1>
-<h2 class="city">New York</h2>
+<h3 class="description"></h3>
 <div class="details">
   <div class="col">
     <img src="images/humidity.png" alt="">
@@ -25,6 +27,20 @@ const weatherContent = `
       <p>Wind Speed</p>
     </div>
   </div>
+  <div class="col">
+    <img src="images/pressure.png" alt="">
+    <div>
+      <p class="pressure"></p>
+      <p>Pressure</p>
+    </div>
+  </div>
+  <div class="col">
+    <img src="images/visibility.png" alt="">
+    <div>
+      <p class="visibility"></p>
+      <p>Visibility</p>
+    </div>
+  </div>
 </div>`;
 
 const renderWeather = () => {
@@ -32,6 +48,7 @@ const renderWeather = () => {
     resolve(weatherDiv.innerHTML = weatherContent);
   })
 }
+
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + `&q=${city}` + `&appid=${apiKey}`);
@@ -41,11 +58,15 @@ async function checkWeather(city) {
     return;
   }
   const data = await response.json();
+  console.log(data);
   await renderWeather();
   const weatherIcon = document.querySelector(".weather-icon")
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".description").innerHTML = data.weather[0].main;
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".pressure").innerHTML = data.main.pressure + "hPa";
+  document.querySelector(".visibility").innerHTML = data.visibility / 1000 + " " + "km";
   document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + "km/h";
 
   if (data.weather[0].main == "Clouds") {
@@ -64,4 +85,10 @@ async function checkWeather(city) {
 
 searchBtn.addEventListener("click",() => {
   checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keydown",(e) => {
+  if (e.keyCode === 13) {
+    checkWeather(searchBox.value);
+  }
 });
